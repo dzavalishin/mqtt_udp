5e977f333aa83f2650b5861754ff463e260e771d
Dmitry Zavalishin <dz@dz.ru>
2017-12-24 01:18:08 +0300

    Java sub follow mode

--- java/src/ru/dz/mqtt_udp/util/Sub.java
+++ java/src/ru/dz/mqtt_udp/util/Sub.java
@@ -1,6 +1,7 @@
 package ru.dz.mqtt_udp.util;
 
 import java.io.IOException;
+import java.net.DatagramSocket;
 import java.net.SocketException;
 
 import ru.dz.mqtt_udp.IPacket;
@@ -9,8 +10,29 @@ import ru.dz.mqtt_udp.PublishPacket;
 
 public class Sub {
 
-	public static void main(String[] args) throws SocketException, IOException, MqttProtocolException {
+	public static void main(String[] args) throws SocketException, IOException, MqttProtocolException 
+	{
+		if( args.length == 1 && args[0].equalsIgnoreCase("-f"))
+		{
+			loop();
+			System.exit(0);
+		}
+		
 		IPacket p = GenericPacket.recv();
+		processPacket(p);
+	}
+
+	private static void loop() throws IOException, MqttProtocolException {
+		DatagramSocket s = GenericPacket.recvSocket();
+		
+		while(true)
+		{
+			IPacket p = GenericPacket.recv(s);
+			processPacket(p);
+		}
+	}
+
+	private static void processPacket(IPacket p) {
 		System.out.println(p);
 		
 		if (p instanceof PublishPacket) {
@@ -18,6 +40,7 @@ public class Sub {
 			
 			// now use pp.getTopic() and pp.getValueString() or pp.getValueRaw()
 		}
+		
 	}
 
 }
