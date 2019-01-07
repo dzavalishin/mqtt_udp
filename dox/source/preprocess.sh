af075d517a9391ba20f09791ebabf9620c3050c6
Dmitry Zavalishin <dz@dz.ru>
2019-01-07 20:55:51 +0300

    python3 version number update

--- dox/source/preprocess.sh
+++ dox/source/preprocess.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 #gawk -f preprocess.awk -v ver=0.55 conf.py.in  |grep version
 # pass version number as 1st arg
-gawk -f preprocess.awk -v ver=$1 -v rel=$1-$2 conf.py.in >conf.py 
+gawk -f ../../common/bin/preprocess.awk -v ver=$1 -v rel=$1-$2 conf.py.in >conf.py 
--- lang/lua/preprocess.awk
+++
@@ -1,7 +0,0 @@
-#BEGIN { if(ARGC < 2) }
-
-#! /@version@/ { print $0 }
-  /@version@/ { sub( "@version@", ver, $0 ); print $0; next }
-  /@release@/ { sub( "@release@", rel, $0 ); print $0; next }
-
-  { print $0 }
