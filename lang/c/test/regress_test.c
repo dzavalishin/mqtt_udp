1449d0ecdd4dc42d1e864dbbe75a9aa42565b971
Dmitry Zavalishin <dz@dz.ru>
2019-01-11 01:28:47 +0300

    c tests
    See #36

--- lang/c/test/regress_test.c
+++ lang/c/test/regress_test.c
@@ -1,12 +1,65 @@
 
+#include <stdio.h>
+
 #include "../mqtt_udp.h"
+
+
+void assertTrue( int i )
+{
+    if( !i )
+    {
+        printf("FAILED!\n");
+        exit(33);
+    }
+}
+
+void assertFalse( int i ) { assertTrue( !i ); }
+
+
+
+
+void testPlain()
+{
+    char * tf = "aaa/ccc/bbb";
+    printf("testPlain ");
+    assertTrue( mqtt_udp_match( tf, "aaa/ccc/bbb") );
+    assertFalse( mqtt_udp_match( tf, "aaa/c/bbb") );
+    assertFalse( mqtt_udp_match( tf, "aaa/ccccc/bbb") );
+    assertFalse( mqtt_udp_match( tf, "aaa/ccccc/ccc") );
+    printf("PASSED\n");
+}
+
+
+void testPlus()
+{
+    char * tf = "aaa/+/bbb";
+    printf("testPlus ");
+    assertTrue( mqtt_udp_match( tf, "aaa/ccc/bbb") );
+    assertTrue( mqtt_udp_match( tf, "aaa/c/bbb") );
+    assertTrue( mqtt_udp_match( tf, "aaa/ccccc/bbb") );
+    assertFalse( mqtt_udp_match( tf, "aaa/ccccc/ccc") );
+    printf("PASSED\n");
+}
+
+
+void testSharp()
+{
+    char * tf = "aaa/#";
+    printf("testSharp ");
+    assertTrue( mqtt_udp_match( tf, "aaa/ccc/bbb") );
+    assertTrue( mqtt_udp_match( tf, "aaa/c/bbb") );
+    assertTrue( mqtt_udp_match( tf, "aaa/ccccc/bbb") );
+    assertFalse( mqtt_udp_match( tf, "aba/ccccc/ccc") );
+    printf("PASSED\n");
+}
+
 
 int main()
 {
 
-
-
-
+    testPlain();
+    testPlus();
+    testSharp();
 
 
 }
