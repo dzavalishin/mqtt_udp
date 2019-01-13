4ea6f827469ed963e1a61bc7b0ac19a76073e094
Dmitry Zavalishin <dz@dz.ru>
2019-01-13 13:15:39 +0300

    done on pypi
    close #6
    See <https://pypi.org/project/mqttudp/>

--- lang/python3/upload.sh
+++ lang/python3/upload.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-#./setup.py sdist upload
-#python ./setup.py sdist upload -r https://upload.pypi.org/legacy/
-#python3 setup.py sdist bdist_wheel
 
-twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# test site upload
+#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+# main site upload
+twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+
