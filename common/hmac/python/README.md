b961382cefe9341dfa7e261e7b6ad73c93746883
Dmitry Zavalishin <dz@dz.ru>
2019-01-17 15:11:29 +0300

    python hmac test
    See #19

---
+++ common/hmac/python/README.md
@@ -0,0 +1,19 @@
+# HMAC - digital signature
+
+
+key = "key" 6B657900
+text = "text"
+
+## MD5 based HMAC
+
+my d0ca6177c61c975fd2f8c07d8c6528c6
+ok d0ca6177c61c975fd2f8c07d8c6528c6
+
+
+## SHA-256 based HMAC
+
+
+my 6afa9046a9579cad143a384c1b564b9a250d27d6f6a63f9f20bf3a7594c9e2c6
+ok 6afa9046a9579cad143a384c1b564b9a250d27d6f6a63f9f20bf3a7594c9e2c6
+
+
---
+++ common/hmac/python/hmac_test.py
@@ -0,0 +1,31 @@
+import hashlib
+import hmac
+
+secret="key"
+data="text"
+
+sig_sha256 =  hmac.new(
+    key=secret.encode('utf-8'),
+    msg=data.encode('utf-8'),
+    digestmod=hashlib.sha256
+).hexdigest()
+
+print( sig_sha256 )
+
+if sig_sha256 != "6afa9046a9579cad143a384c1b564b9a250d27d6f6a63f9f20bf3a7594c9e2c6":
+    print("ERROR!")
+else:
+    print("OK")
+
+sig_md5 =  hmac.new(
+    key=secret.encode('utf-8'),
+    msg=data.encode('utf-8'),
+    digestmod=hashlib.md5
+).hexdigest()
+
+print( sig_md5 )
+
+if sig_md5 != "d0ca6177c61c975fd2f8c07d8c6528c6":
+    print("ERROR!")
+else:
+    print("OK")
