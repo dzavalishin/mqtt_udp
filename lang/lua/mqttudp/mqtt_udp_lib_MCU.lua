9c428705b57b16a9c1d063eca03686abdbcb58ad
Dmitry Zavalishin <dz@dz.ru>
2019-01-20 14:56:00 +0300

    listen code loads but not working

--- lang/lua/mqttudp/mqtt_udp_lib_MCU.lua
+++ lang/lua/mqttudp/mqtt_udp_lib_MCU.lua
@@ -21,14 +21,14 @@ function mqtt_udp_lib.make_listen_socket()
     local udpSocket, our_ip
 
 	our_ip = wifi.sta.getip() -- we store our_ip, as we need it to setup udp socket properly
-	print(" Your IP is ".. our_ip)
+	print("Listen on IP ".. our_ip)
 
     --udp = socket.udp()
     udpSocket = net.createUDPSocket() -- NodeMCU way
     --udp:setsockname("*", defs.MQTT_PORT )
     udpSocket:listen( defs.MQTT_PORT, our_ip ) -- NodeMCU way
     -- udp:settimeout(1)
-    udp:settimeout()
+    --udpSocket:settimeout() -- no method
 
     return udpSocket
 
