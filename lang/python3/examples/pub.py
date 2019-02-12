b1149ea1a765d0582a94fb0d78a7cec0e5cad4ae
Dmitry Zavalishin <dz@dz.ru>
2019-02-12 19:19:06 +0300

    Update pub.py
    See #128

--- lang/python3/examples/pub.py
+++ lang/python3/examples/pub.py
@@ -5,12 +5,29 @@
 '''
 
 # will work even if package is not installed 
+import argparse
 import sys
 sys.path.append('..')
-#sys.path.append('../mqttudp')
-
 import mqttudp.engine as me
 
 if __name__ == "__main__":
     #me.set_signature( "signPassword" )
-    me.send_publish( sys.argv[1], sys.argv[2] )
+
+    parser = argparse.ArgumentParser(description='Send MQTT/UDP publish message',prog='pub')
+    #parser.add_argument('topic', metavar='T', nargs='1', help='topic to send to')
+    #parser.add_argument('value', metavar='V', nargs='1', help='value to be sent')
+    
+    parser.add_argument('topic', help='topic to send to')
+    parser.add_argument('value', help='value to be sent')
+    parser.add_argument('-s', '--signature',  dest='signature', action='store', help='digital signature key')
+    
+    args = parser.parse_args()
+
+    if args.signature != None:
+        me.set_signature( args.signature )
+        print("Signature is '"+args.signature+"'")
+
+    print("Topic  is "+args.topic)
+    print("Value  is "+args.value)
+
+    me.send_publish( args.topic, args.value )
