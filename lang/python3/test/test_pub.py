4877d7fb7361f57e7f1f6699e339b119fb0783ee
Dmitry Zavalishin <dz@dz.ru>
2019-02-12 19:22:55 +0300

    Update test_pub.py

--- lang/python3/test/test_pub.py
+++ lang/python3/test/test_pub.py
@@ -6,11 +6,12 @@
 '''
 
 # will work even if package is not installed 
+import argparse
 import sys
 sys.path.append('..')
 sys.path.append('../mqttudp')
 
-import mqttudp.engine
+import mqttudp.engine as me
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #
@@ -20,6 +21,18 @@ import mqttudp.engine
 
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Send MQTT/UDP publish message',prog='test_pub')
+    
+    parser.add_argument('topic', help='topic to send to')
+    parser.add_argument('value', help='value to be sent')
+    parser.add_argument('-s', '--signature',  dest='signature', action='store', help='digital signature key')
+    
+    args = parser.parse_args()
+
+    if args.signature != None:
+        me.set_signature( args.signature )
+
     print( "Will publish to '"+sys.argv[1] + "' value '" + sys.argv[2] + "'")
-    mqttudp.engine.send_publish( sys.argv[1], sys.argv[2] )
+    me.send_publish( args.topic, args.value )
     print( "Sent ok")
