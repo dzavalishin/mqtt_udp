7e4564b786a254646042aa5bf8f0e0d085c218e3
Dmitry Zavalishin <dz@dz.ru>
2023-10-16 20:08:06 +0300

    Create parse_test.go

---
+++ lang/go/dz.ru/mqttudp/parse_test.go
@@ -0,0 +1,46 @@
+package mqttudp
+
+import (
+	//"log"
+
+	"testing"
+)
+
+type myServer struct {
+	t *testing.T
+}
+
+func (s myServer) Accept(packet MqttPacket) {
+	//fmt.Println("got pkt ", packet)
+	packet.Dump()
+}
+
+func Test_Parse_Publish(t *testing.T) {
+	var raw = []byte{0x30, 8, 0, 4, 'a', 'b', 'c', 'd', 'x', 'y'}
+
+	Parse_any_pkt(raw, nil, nil)
+}
+
+func Test_Parse_Publish_TTR(t *testing.T) {
+	var raw = []byte{0x30, 8, 0, 4, 'a', 'b', 'c', 'd', 'x', 'y' /*TTR*/, 'n', 4, 0, 0, 0, 1}
+
+	Parse_any_pkt(raw, nil, nil)
+}
+
+func Test_Parse_Publish_callback(t *testing.T) {
+	var raw = []byte{0x30, 8, 0, 4, 'a', 'b', 'c', 'd', 'x', 'y'}
+
+	var cb myServer
+	cb.t = t
+
+	Parse_any_pkt(raw, nil, cb)
+}
+
+func Test_Parse_Publish_TTR_callback(t *testing.T) {
+	var raw = []byte{0x30, 8, 0, 4, 'a', 'b', 'c', 'd', 'x', 'y' /*TTR*/, 'n', 4, 0, 0, 0, 1}
+
+	var cb myServer
+	cb.t = t
+
+	Parse_any_pkt(raw, nil, cb)
+}
